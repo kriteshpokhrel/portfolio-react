bd5a31791e0804121870ca83e5a4e8eb197bdba7
Kritesh Pokhrel <kriteshpokharel100@gmail.com>
2025-12-07 17:26:20 +0545

    use Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
 import { useState } from "react";
 import "./App.css";
-import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
-import { Home } from "./components/sections/Home";
-import { About } from "./components/sections/About";
-import { Projects } from "./components/sections/Projects";
+import { HomePage } from "./components/homepage/HomePage";
+import BlogList from "./components/blogs/BlogList";
 import "./index.css";
-import { Contact } from "./components/sections/Contact";
+import { Routes, Route } from "react-router-dom";
+import BlogPostPage from "./components/blogs/BlogPostPage";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-
+  const isHome = location.pathname === "/";
+  console
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
-      <div
-        className={`min-h-screen transition-opacity duration-700 ${
-          isLoaded ? "opacity-100" : "opacity-0"
-        } bg-black text-gray-100`}
-      >
+      <div>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <Home />
-        <About />
-        <Projects />
-        <Contact />
+        { isHome && <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} /> }
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blogs" element={<BlogList />} />
+          <Route path="/blogs/:slug" element={<BlogPostPage />} />
+        </Routes>
       </div>
     </>
   );
